5d680b3c278f66ccfdee0fef4cf4d9ae24dbb6e7
Kenneth <kenneth.h.rhee@gmail.com>
2014-03-30 23:36:31 -0400

    Signed-off-by: Kenneth <kenneth.h.rhee@gmail.com>

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -8,16 +8,46 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebResponse;
+import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+
 public class HelloServlet extends HttpServlet {
 
 	@Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+		/*
         ServletOutputStream out = resp.getOutputStream();
         
         out.write("Hello Heroku".getBytes());
         out.flush();
         out.close();
+        */
+		String URL = "https://ps01.bergen.org/public/home.html";
+		WebClient webClient = new WebClient();
+		webClient.setThrowExceptionOnFailingStatusCode(false);
+		webClient.setThrowExceptionOnScriptError(false);
+		HtmlPage page = webClient.getPage(URL);
+		WebResponse response = page.getWebResponse();
+		String content = response.getContentAsString();
+		
+		HtmlForm form = page.getFormByName("LoginForm");
+		HtmlButton button = form.getButtonByName("value");
+		HtmlTextInput username = form.getInputByName("account");
+		HtmlTextInput password = form.getInputByName("pw");
+		username.setValueAttribute("hwarhe");
+		password.setValueAttribute("9wg3Bg!");
+		HtmlPage landing = button.click();
+		
+		ServletOutputStream output = resp.getOutputStream();
+		String testcase = "\nTest case: " + content;
+		output.write(testcase.getBytes());
+		output.flush();
+		output.close();
+		webClient.closeAllWindows();
     }
-    
 }
