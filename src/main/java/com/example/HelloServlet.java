7f01a578365c1081b7832675ff26c80401e65961
Kenneth <kenneth.h.rhee@gmail.com>
2014-04-01 18:27:10 -0400

    Signed-off-by: Kenneth <kenneth.h.rhee@gmail.com>

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -57,11 +57,9 @@ public class HelloServlet extends HttpServlet {
 		*/
 		Element table = page.select("table").first();
 		for (Element row : table.select("tr")) {
-			builder.append("\n[COLUMN] ");
 			for (Element column : row.select("td")) {
 				builder.append(column + "<<");
 			}
-			builder.append("[END COLUMN]\n");
 		}
 		String contents = builder.toString();
 		
