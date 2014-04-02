078aec570509d28edd1db4375153eb7e34522c99
Kenneth <kenneth.h.rhee@gmail.com>
2014-04-02 00:35:14 -0400

    Signed-off-by: Kenneth <kenneth.h.rhee@gmail.com>

--- src/main/java/com/heroku/bcagpa/GPAServlet.java
+++ src/main/java/com/heroku/bcagpa/GPAServlet.java
@@ -71,6 +71,7 @@ public class GPAServlet extends HttpServlet {
 	
 	private void calculate() {
 		StringBuilder builder = new StringBuilder();
+		builder.append("<html>");
 		Element table = page.select("table").first();
 		for (int i = 0; i < table.select("tr").size(); i++) {
 			Element cRow = table.select("tr").get(i);
@@ -79,6 +80,7 @@ public class GPAServlet extends HttpServlet {
 				builder.append("<br>" + subject.get(0).text() + subject.get(11).text() + " GRADES:" + subject.get(12).text() + "," + subject.get(13).text() + "," + subject.get(14).text());
 			}
 		}
+		builder.append("</html>");
 		this.output = builder.toString();
 	}
 	
