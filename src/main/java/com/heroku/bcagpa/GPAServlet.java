79242286247b00941e166ec92a68194ea8e8d70a
kenrhe <kenneth.h.rhee@gmail.com>
2014-04-19 22:49:26 -0400

    Signed-off-by: kenrhe <kenneth.h.rhee@gmail.com>

--- src/main/java/com/heroku/bcagpa/GPAServlet.java
+++ src/main/java/com/heroku/bcagpa/GPAServlet.java
@@ -3,20 +3,11 @@ package com.heroku.bcagpa;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 public class GPAServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
