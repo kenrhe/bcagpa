127db3fa023840336b53e6a445f5df86a6b02667
kenrhe <kenneth.h.rhee@gmail.com>
2014-04-22 18:07:09 -0400

    Signed-off-by: kenrhe <kenneth.h.rhee@gmail.com>

--- src/main/java/com/heroku/bcagpa/AboutServlet.java
+++ src/main/java/com/heroku/bcagpa/AboutServlet.java
@@ -3,10 +3,11 @@ package com.heroku.bcagpa;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class AboutServlet {
+public class AboutServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		req.getRequestDispatcher("about.jsp").forward(req, resp);
