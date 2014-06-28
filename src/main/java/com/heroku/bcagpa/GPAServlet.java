10a3527c153806431330073458f824b58de86a91
Kenneth Rhee <kenneth.h.rhee@gmail.com>
2014-06-27 22:35:02 -0400

    Signed-off-by: Kenneth Rhee <kenneth.h.rhee@gmail.com>

--- src/main/java/com/heroku/bcagpa/GPAServlet.java
+++ src/main/java/com/heroku/bcagpa/GPAServlet.java
@@ -22,7 +22,7 @@ public class GPAServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		try {
-			String username = req.getParameter("username").replace("@bergen.org", "");
+			String username = req.getParameter("username").replace("@bergen.org", "").replace("bca/", "");
 			String password = req.getParameter("password");
 			GPA user = new GPA(username, password);
 			req.setAttribute("tri1GPA", round(user.getTriOneGPA(), 3));
